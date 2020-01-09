87ebd99905002e802bf4fb701c10716d5185934a
ReBorn67 <49155130+ReBorn67@users.noreply.github.com>
2020-01-09 09:42:30 +0900

    path

--- layouts/AppHeader.vue
+++ layouts/AppHeader.vue
@@ -1,14 +1,14 @@
 <template>
     <header class="header-global sticky-top">
         <base-nav id="header" class="navbar-main" transparent type="" effect="light" expand>
-            <router-link slot="brand" class="navbar-brand mr-lg-5" to="/">
-                <img src="/img/brand/white.png" alt="logo">
+            <router-link slot="brand" class="navbar-brand mr-lg-5" :to="HOME_PATH">
+                <img :src="HOME_PATH+'img/brand/white.png'" alt="logo">
             </router-link>
 
             <div class="row" slot="content-header" slot-scope="{closeMenu}">
                 <div class="col-6 collapse-brand">
                     <a href="https://demos.creative-tim.com/vue-argon-design-system/documentation/">
-                        <img src="/img/brand/blue.png">
+                        <img :src="HOME_PATH+'img/brand/blue.png'">
                     </a>
                 </div>
                 <div class="col-6 collapse-close">
@@ -52,11 +52,11 @@
                         <i class="ni ni-collection d-lg-none"></i>
                         <span class="nav-link-inner--text">Examples</span>
                     </a>
-                    <router-link to="/landing" class="dropdown-item">Landing</router-link>
-                    <router-link to="/profile" class="dropdown-item">Profile</router-link>
-                    <router-link to="/login" class="dropdown-item">Login</router-link>
-                    <router-link to="/register" class="dropdown-item">Register</router-link>
-                    <router-link to="/about" class="dropdown-item">about</router-link>
+                    <router-link :to="HOME_PATH+'landing'" class="dropdown-item">Landing</router-link>
+                    <router-link :to="HOME_PATH+'profile'" class="dropdown-item">Profile</router-link>
+                    <router-link :to="HOME_PATH+'login'" class="dropdown-item">Login</router-link>
+                    <router-link :to="HOME_PATH+'register'" class="dropdown-item">Register</router-link>
+                    <router-link :to="HOME_PATH+'about'" class="dropdown-item">about</router-link>
                 </base-dropdown>
             </ul>
             <ul class="navbar-nav align-items-lg-center ml-lg-auto">
@@ -114,6 +114,7 @@ export default {
     },
     data () {
         return {
+            HOME_PATH: process.env.HOME_PATH
         };
     },
     methods: {
