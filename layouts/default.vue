8b28a965eabd188b2ac726e1d1ee9c5e09010d02
ReBorn67 <49155130+ReBorn67@users.noreply.github.com>
2020-01-10 15:05:19 +0900

    layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,10 +3,10 @@
         <app-header name="header"></app-header>
         <main class="mt-4">
             <div class="container">
-                <div class="row ml-auto">
+                <div class="row">
                     <app-left-side name="left-side"></app-left-side>
 
-                    <fade-transition class="col col-lg-9" origin="center" mode="out-in" :duration="250">
+                    <fade-transition class="col col-lg-9 ml-auto" origin="center" mode="out-in" :duration="250">
                         <div id="content"><nuxt /></div>
                     </fade-transition>
                 </div>
