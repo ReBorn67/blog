a0aee4baffcd6a8098f71ee65b3820a18a97608a
ReBorn67 <49155130+ReBorn67@users.noreply.github.com>
2020-02-04 09:57:54 +0900

    color

--- layouts/AppFooter.vue
+++ layouts/AppFooter.vue
@@ -25,13 +25,13 @@
         <div class="col-md-4 mb-5 mb-lg-0">
           <card class="footer-block border-0" shadow body-classes="py-5">
             <div class="d-flex mb-4">
-              <icon name="fa fa-comments" type="theme2" rounded></icon>
-              <h6 class="text-theme2 text-uppercase mb-0 ml-4 align-self-center">Recent Comments</h6>
+              <icon name="fa fa-comments" type="theme3" rounded></icon>
+              <h6 class="text-theme3 text-uppercase mb-0 ml-4 align-self-center">Recent Comments</h6>
             </div>
 
             <div>
               <a v-for="n in 6" href="#" class="d-flex w-100 footer-links">
-                <h6 class="text-theme2">
+                <h6 class="text-theme3">
                   <small><i class="fa fa-circle" aria-hidden="false"></i></small>
                   <span class="text-truncate">recent comment {{ n }}</span>
                 </h6>
@@ -45,28 +45,28 @@
         <div class="col-md-4 mb-5 mb-lg-0">
           <card class="footer-block border-0" shadow body-classes="py-5">
             <div class="d-flex mb-4">
-              <icon name="fa fa-tags" type="theme3" rounded></icon>
-              <h6 class="text-theme3 text-uppercase mb-0 ml-4 align-self-center">Tags</h6>
+              <icon name="fa fa-tags" type="theme5" rounded></icon>
+              <h6 class="text-theme5 text-uppercase mb-0 ml-4 align-self-center">Tags</h6>
             </div>
 
             <div class="d-flex flex-wrap">
-              <a href="#" class="ml-0 mr-1 mb-1 badge badge-pill badge-theme3">marketing</a>
-              <a href="#" class="ml-0 mr-1 mb-1 badge badge-pill badge-theme3">product</a>
-              <a href="#" class="ml-0 mr-1 mb-1 badge badge-pill badge-theme3">launch</a>
-              <a href="#" class="ml-0 mr-1 mb-1 badge badge-pill badge-theme3">richesse</a>
-              <a href="#" class="ml-0 mr-1 mb-1 badge badge-pill badge-theme3">yearlong</a>
-              <a href="#" class="ml-0 mr-1 mb-1 badge badge-pill badge-theme3">ferruginous</a>
-              <a href="#" class="ml-0 mr-1 mb-1 badge badge-pill badge-theme3">benweed</a>
-              <a href="#" class="ml-0 mr-1 mb-1 badge badge-pill badge-theme3">indusioid</a>
-              <a href="#" class="ml-0 mr-1 mb-1 badge badge-pill badge-theme3">recursion</a>
-              <a href="#" class="ml-0 mr-1 mb-1 badge badge-pill badge-theme3">subcontrarily</a>
-              <a href="#" class="ml-0 mr-1 mb-1 badge badge-pill badge-theme3">uncolouredly</a>
-              <a href="#" class="ml-0 mr-1 mb-1 badge badge-pill badge-theme3">tuza</a>
-              <a href="#" class="ml-0 mr-1 mb-1 badge badge-pill badge-theme3">mozambique</a>
-              <a href="#" class="ml-0 mr-1 mb-1 badge badge-pill badge-theme3">equiformity</a>
-              <a href="#" class="ml-0 mr-1 mb-1 badge badge-pill badge-theme3">proctoplasty</a>
-              <a href="#" class="ml-0 mr-1 mb-1 badge badge-pill badge-theme3">ophthalmomalacia</a>
-              <a href="#" class="ml-0 mr-1 mb-1 badge badge-pill badge-theme3">subalternately</a>
+              <a href="#" class="ml-0 mr-1 mb-1 badge badge-pill badge-theme5">marketing</a>
+              <a href="#" class="ml-0 mr-1 mb-1 badge badge-pill badge-theme5">product</a>
+              <a href="#" class="ml-0 mr-1 mb-1 badge badge-pill badge-theme5">launch</a>
+              <a href="#" class="ml-0 mr-1 mb-1 badge badge-pill badge-theme5">richesse</a>
+              <a href="#" class="ml-0 mr-1 mb-1 badge badge-pill badge-theme5">yearlong</a>
+              <a href="#" class="ml-0 mr-1 mb-1 badge badge-pill badge-theme5">ferruginous</a>
+              <a href="#" class="ml-0 mr-1 mb-1 badge badge-pill badge-theme5">benweed</a>
+              <a href="#" class="ml-0 mr-1 mb-1 badge badge-pill badge-theme5">indusioid</a>
+              <a href="#" class="ml-0 mr-1 mb-1 badge badge-pill badge-theme5">recursion</a>
+              <a href="#" class="ml-0 mr-1 mb-1 badge badge-pill badge-theme5">subcontrarily</a>
+              <a href="#" class="ml-0 mr-1 mb-1 badge badge-pill badge-theme5">uncolouredly</a>
+              <a href="#" class="ml-0 mr-1 mb-1 badge badge-pill badge-theme5">tuza</a>
+              <a href="#" class="ml-0 mr-1 mb-1 badge badge-pill badge-theme5">mozambique</a>
+              <a href="#" class="ml-0 mr-1 mb-1 badge badge-pill badge-theme5">equiformity</a>
+              <a href="#" class="ml-0 mr-1 mb-1 badge badge-pill badge-theme5">proctoplasty</a>
+              <a href="#" class="ml-0 mr-1 mb-1 badge badge-pill badge-theme5">ophthalmomalacia</a>
+              <a href="#" class="ml-0 mr-1 mb-1 badge badge-pill badge-theme5">subalternately</a>
             </div>
           </card>
         </div>
